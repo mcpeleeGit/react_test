b8f0890604dd1a2505e26934167ac3423543024f
mcpeleeGit <mcpelee@gmail.com>
2022-12-21 00:15:33 +0900

    set

--- components/utterances.js
+++ components/utterances.js
@@ -1,5 +1,19 @@
 export default function Utterances(){
     return (
-        <section/>
+        <section
+            ref={(elem)=> {
+                if(!elem) {
+                    return
+                }
+                const scriptElement = document.createElement('script')
+                scriptElement.src = "https://utteranc.es/client.js"
+                scriptElement.async = true
+                scriptElement.setAttribute('repo','mcpeleeGit/react_test')
+                scriptElement.setAttribute('issue-term','pathname')
+                scriptElement.setAttribute('theme','github-light')
+                scriptElement.crossOrigin = 'anonymous'
+                elem.appendChild(scriptElement)
+            }}
+        />
     )
 }
